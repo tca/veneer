be7eb588de66ee84d60bc8bd10094ad234915c5b
tca <theconartist@gmail.com>
2015-02-01 21:50:26 -0600

    start on reader

---
+++ reader.js
@@ -0,0 +1,166 @@
+var eof = ["eof"];
+function eofp(c) { return eof === c; }
+
+function stream(string) {
+    var length = string.length;
+    this.pos = 0;
+    this.line = 1;
+    this.string = string;
+    this.peek_char = function() {
+        return (this.pos >= length) ? eof : string[this.pos];
+    }
+    this.read_char = function() {
+        var old_pos = this.pos;
+        this.pos++;
+        if (string[old_pos] == "\n") { line++; }
+        return (old_pos >= length) ? eof : string[old_pos];
+    }
+    this.unread_char = function() {
+        if (string[this.pos-1] == "\n") { line--; }
+        this.pos--;
+    }
+    this.fork = function() {
+        var fork = new stream(string);
+        fork.pos = this.pos;
+        fork.line = this.line;
+        return fork;
+    }
+}
+
+function dotp(c) { return c == "."; }
+function whitespacep(c) { return /\s/.test(c); }
+function symbolicp(c) { return /[a-zA-Z0-9=\/\\!@#$%^&*_+=-?.~<>]/.test(c); }
+function number_stringp(cs) { return /[0-9]+/.test(cs.join("")); }
+
+function skip_whitespace(input_stream) {
+    var c;
+    while(true) {
+        c = input_stream.read_char();
+        if(!whitespacep(c)) { break; }
+    }
+    input_stream.unread_char();
+}
+
+// intern symbol using table and [] for fresh
+function read_symbol(input_stream) {
+    var buffer = [input_stream.read_char()];
+    var c;
+    while(true) {
+        c = input_stream.read_char();
+        if(!eofp(c) && symbolicp(c)) {
+            buffer.push(c);
+        } else {
+            input_stream.unread_char();
+            return buffer.join("");
+        }
+    }
+}
+
+function read_string(input_stream) {
+    var buffer = [];
+    var c;
+    input_stream.read_char();
+    while(c = input_stream.read_char()) {
+        if(c == '"') {
+            return buffer.join("");
+        } else if (c == "\\") {
+            buffer.push(input_stream.read_char());
+        } else {
+            buffer.push(c);
+        }
+    }
+}
+
+// todo: don't automatically read next
+function read_whitespace(input_stream) {
+    input_stream.read_char();
+    skip_whitespace(input_stream);
+    return read(input_stream);
+}
+
+function read_until_end_of_line(input_stream) {
+    while(input_stream.read_char() != "\n") {}
+}
+
+// todo: don't automatically read next
+function read_comment(input_stream) {
+    read_until_end_of_line(input_stream);
+    return read(input_stream);
+}
+
+// todo: intern quote
+function read_quoted(input_stream) {
+    input_stream.read_char();
+    return ["quote", read(input_stream)];
+}
+
+// todo: intern quasiquote
+function read_quasiquoted(input_stream) {
+    input_stream.read_char();
+    return ["quasiquote", read(input_stream)];
+}
+
+// todo: intern unquote
+function read_unquoted(input_stream) {
+    input_stream.read_char();
+    return ["unquote", read(input_stream)];
+}
+
+function read_list(input_stream) {
+    input_stream.read_char();
+    return read_all([], input_stream);
+}
+
+function read_hash(input_stream) {
+    input_stream.read_char();
+    switch (input_stream.read_char()) {
+    case "f": return false;
+    case "t": return true;
+    case "\\": return input_stream.read_char();
+    default: console.log("unknown hash code");
+    }
+}
+
+function reader_for(c) {
+    switch(c) {
+    case '"': return read_string;
+    case "'": return read_quoted;
+    case "`": return read_quasiquoted;
+    case ",": return read_unquoted;
+    case "(": return read_list;
+    case ";": return read_comment;
+    case "#": return read_hash;
+    }
+    if (whitespacep(c)) { return read_whitespace; }
+    if (symbolicp(c)) { return read_symbol; }
+    return false;
+}
+
+function read(input_stream) {
+    var c = input_stream.peek_char();
+    var reader = reader_for(c);
+    if(reader) { return reader(input_stream); }
+}
+
+// todo: use conses instead of arrays
+function read_all(sexps, input_stream) {
+    skip_whitespace(input_stream);
+    var c = input_stream.peek_char();
+    if (eofp(c) || c == ")") {
+        input_stream.read_char();
+        return sexps;
+    } else if (dotp(input_stream.peek_char())) {
+        input_stream.read_char();
+        var result = sexps.concat(read(input_stream));
+        skip_whitespace(input_stream);
+        var c = input_stream.peek_char();
+        if (eofp(c)|| c == ")") {
+            input_stream.read_char();
+        } else {
+            console.log("stuff after dot");
+        }
+        return result;
+    } else {
+        return read_all(sexps.concat([read(input_stream)]), input_stream);
+    }
+}
