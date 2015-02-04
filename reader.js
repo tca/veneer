80835f23aec93b2e3a9805755b15a1d4d6395e82
tca <theconartist@gmail.com>
2015-02-04 12:01:31 -0600

    use conses and intern symbols

--- reader.js
+++ reader.js
@@ -1,12 +1,29 @@
-// todo: add parse result type so any reader can error
-//       for example if it hits an unexpected eof
+
+function pair(car, cdr) {
+    this.car = car;
+    this.cdr = cdr;
+}
+function cons(car,cdr) { return new pair(car,cdr); }
+
+var symbol_table = new Object(null);
+
+function Symbol(str) { this.string = str; }
+function intern(string) {
+    var sym = new Symbol(string);
+    symbol_table[string] = sym;
+    return sym;
+}
+
+function reverse(lst,memo) {
+    if (lst == null) { return memo }
+    else { return reverse(lst.cdr, cons(lst.car, memo)); }
+}
 
 var eof = ["eof"];
 function eofp(c) { return eof === c; }
 
-// todo: make all uses of read_char and peek_char match cases for
-//       eof | char of string
-function stream(string) {
+// todo: functional streams?
+function Stream(string) {
     var length = string.length;
     this.pos = 0;
     this.line = 1;
@@ -25,14 +42,18 @@ function stream(string) {
         this.pos--;
     }
     this.fork = function() {
-        var fork = new stream(string);
+        var fork = new Stream(string);
         fork.pos = this.pos;
         fork.line = this.line;
         return fork;
     }
 }
 
-function dotp(c) { return c == "."; }
+function condition(args) {
+    this.resume = args.resume;
+    this.msg = args.msg;
+}
+
 function whitespacep(c) { return /\s+/.test(c); }
 function symbolicp(c) { return /[a-zA-Z0-9=\/\\!@#$%^&*_+=-?.~<>]/.test(c); }
 function number_stringp(cs) { return /[0-9]+/.test(cs.join("")); }
@@ -46,7 +67,6 @@ function skip_whitespace(input_stream) {
     input_stream.unread_char();
 }
 
-// intern symbol using table and [] for fresh
 function read_symbol(input_stream) {
     var buffer = [input_stream.read_char()];
     var c;
@@ -56,7 +76,7 @@ function read_symbol(input_stream) {
             buffer.push(c);
         } else {
             input_stream.unread_char();
-            return buffer.join("");
+            return intern(buffer.join(""));
         }
     }
 }
@@ -92,54 +112,60 @@ function read_comment(input_stream) {
     return read(input_stream);
 }
 
-// todo: intern quote
 function read_quoted(input_stream) {
     input_stream.read_char();
-    return ["quote", read(input_stream)];
+    return cons(intern("quote"), cons(read(input_stream), null));
 }
 
-// todo: intern quasiquote
 function read_quasiquoted(input_stream) {
     input_stream.read_char();
-    return ["quasiquote", read(input_stream)];
+    return cons(intern("quasiquote"), cons(read(input_stream), null));
 }
 
-// todo: intern unquote
 function read_unquoted(input_stream) {
     input_stream.read_char();
-    return ["unquote", read(input_stream)];
+    return cons(intern("unquote"), cons(read(input_stream), null));
 }
 
 function read_list(input_stream) {
     input_stream.read_char();
-    return read_list_aux([], input_stream);
+    return read_list_aux(null, input_stream);
 }
 
 function read_list_aux(sexps, input_stream) {
-   skip_whitespace(input_stream);
-    var c = input_stream.peek_char();
-    if (eofp(c)) {
-        throw "unexpected eof" // partial result
-    } else if (c == ")") {
-        input_stream.read_char();
-        return sexps;
-    } else if (dotp(input_stream.peek_char())) {
-        input_stream.read_char();
-        var result = sexps.concat(read(input_stream));
+    while (true) {
         skip_whitespace(input_stream);
         var c = input_stream.peek_char();
-        if (c == ")") {
+        switch (c) {
+        case eof:
+            throw "unexpected eof";
+        case ")":
             input_stream.read_char();
-        } else if (eofp(c)){
-            throw "unexpected eof"
-        } else {
-            throw "stuff after dot";
+            return reverse(sexps, null);
+        case ".":
+            return read_after_dot(sexps, input_stream);
+        default:
+            sexps = cons(read(input_stream), sexps);
         }
-        return result;
-    } else {
-        skip_whitespace(input_stream);
-        return read_list_aux(sexps.concat([read(input_stream)]), input_stream);
     }
+}
+
+// TODO: resumable
+function read_after_dot(sexps, input_stream) {
+    input_stream.read_char();
+    var last = read(input_stream);
+    skip_whitespace(input_stream);
+    var c = input_stream.peek_char();
+    switch (c) {
+    case eof:
+        throw "unexpected eof";
+    case ")":
+        input_stream.read_char();
+        break;
+    default:
+        throw "too many expressions after dot";
+    }
+    return reverse(sexps, last);
 }
 
 function read_hash(input_stream) {
@@ -148,10 +174,9 @@ function read_hash(input_stream) {
     case "f": return false;
     case "t": return true;
     case "\\": return input_stream.read_char();
-    default: throw "unknown hash code";
+    default: throw { msg: "unknown hash code" };
     }
 }
-
 
 function reader_for(c) {
     switch(c) {
@@ -173,29 +198,25 @@ function read(input_stream) {
     var c = input_stream.peek_char();
     var reader = reader_for(c);
     if(reader) { return reader(input_stream); }
-    throw "no reader for: " + c;
+    throw { msg: "no reader for: " + c };
 }
 
-// todo: use conses instead of arrays
-//       loop for recursive case instead of recursion
-function read_all(sexps, input_stream, t) {
-    skip_whitespace(input_stream);
-    var c = input_stream.peek_char();
-    if (eofp(c)) {
-        input_stream.read_char();
-        return sexps;
-    } else {
+function read_top(sexps, input_stream) {
+    while (true) {
         skip_whitespace(input_stream);
-        return read_all(sexps.concat([read(input_stream)]), input_stream, t);
+        var c = input_stream.peek_char();
+        if (c === eof) {
+            input_stream.read_char();
+            return reverse(sexps, null);
+        } else {
+            sexps = cons(read(input_stream), sexps);
+        }
     }
 }
 
+var read_repl = function(in_stream) { return read_top(null, new Stream(in_stream)); };
 
 function read_string(str) {
-    try {
-        return read_all([], new stream(str), true)
-    }
-    catch(err) {
-        return err;
-    }
+    try { return read_top(null, new Stream(str)); }
+    catch(err) { return err.msg; }
 }
