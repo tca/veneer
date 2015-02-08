8e9610949991e384d6014c365f1c86f0ad7e45a4
tca <theconartist@gmail.com>
2015-02-07 22:33:53 -0600

    some more example tests

--- mk_test.js
+++ mk_test.js
@@ -12,6 +12,7 @@ function sixes(x) {
                 });
 }
 
+
 function node_log(x) { return console.log(require('util').inspect(x, true, 10)); }
 var empty_state = cons(null, 0);
 var test1 = call_fresh(function (q) { return eqeq(q, 5); })(empty_state);
@@ -35,3 +36,42 @@ var foo = conj(call_fresh(function(z) { x.set(z); return eqeq(x.get(), y.get()) 
                call_fresh(function(z) { y.set(z); return eqeq(y.get(), 1) }));
 
 node_log(take(3, foo(empty_state)));
+
+function appendo(l, s, out) {
+    return disj(conj(eqeq(null, l), eqeq(s, out)),
+               call_fresh(function(a) {
+                   return call_fresh(function(d) {
+                       return conj(eqeq(cons(a, d), l),
+                                  call_fresh(function(res) {
+                                      return conj(function(s_c) {
+                                                      return function() {
+                                                          return appendo(d,s,res)(s_c); };
+                                                  }, eqeq(cons(a, res), out));
+                                  })); 
+                   });
+               }));
+}
+
+var call_appendo =
+    call_fresh(function(q) {
+    return call_fresh(function(l) {
+        return call_fresh(function(s) {
+            return call_fresh(function(out) {
+                return conj(appendo(l,s,out),
+                            eqeq(list(l, s), q)); }); }); }); });
+
+function pp(x) {
+    if (pairp(x)) {
+        return ["(", pp(x.car), ".", pp(x.cdr), ")"].join("");
+    } else {
+        return JSON.stringify(x);
+    }
+}
+
+var test3 = call_fresh(function(a) { return appendo(list(1), a, list(1,2)); });
+var test4 = call_fresh(function(q) { return call_fresh(function(b) { return call_fresh(function(a) { return conj(eqeq(list(a,b), q),
+                                                                                                                 appendo(a, b, list(1,2,3))); }); }); });
+
+console.log("--");
+map(function(x) { return node_log(reify_first(x)); }, take(5, test4(empty_state)));
+node_log(eqeq(1,1)(empty_state));
