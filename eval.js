974119fe27b54b998d476d0db8c087b33666a809
orchid.hybrid <orchid.hybrid@gmail.com>
2015-02-18 16:20:54 +0000

    disequality constraint reification

--- eval.js
+++ eval.js
@@ -328,10 +328,34 @@ function VeneerVM() {
         return walk_star(v1, reify_s(v1, Immutable.Map()));
     }
 
-    function query_map(qm, s) {
-        return foldl(query(qm, s), [], function(m, a_v) {
+    function intersperse_map(l, f, sep) {
+        var m = [];
+        while(l != null) {
+            m = m.concat([f(l.car),(l.cdr == null) ? "" : sep]);
+            l = l.cdr;
+        }
+        return m;
+    }
+    
+    function query_map(qm, d, s) {
+        var q = query(cons(qm, d), s);
+        var result = foldl(q.car, [], function(m, a_v) {
             return m.concat([a_v.car, ": ", pretty_print(a_v.cdr), "\n"]);
         });
+        
+        if(false) {
+            return result;
+        }
+        else {
+            var present = function(dd) {
+                // dd is a disjunction of disequalities
+                return pretty_print(cons(intern("or"),map(function(a){return list(intern("=/="),a.car,a.cdr)},dd)));
+            };
+            result = result.concat(["{"]);
+            result = result.concat(intersperse_map(q.cdr, present, ","));
+            result = result.concat(["}", "\n"]);
+            return result;
+        }
     }
 
     function map_stream(fn, stream) {
@@ -350,8 +374,9 @@ function VeneerVM() {
 
         var run_queries = function(mks) {
             var s = mks.substitution;
+            var d = mks.diseq;
             var qm = map(function(p) { return cons(p.car.string, p.cdr()()); }, env);
-            var record = query_map(qm, s);
+            var record = query_map(qm, d, s);
             return record.join("");
         };
         return map_stream(run_queries, $);
