0fda85661012dedccd00ee2152062ea5ad08876f
tca <theconartist@gmail.com>
2015-02-24 22:54:07 -0600

    make saving and switching files a bit more intuitive

--- editor.js
+++ editor.js
@@ -9,7 +9,7 @@ function load_editor() {
     var errors = ed.errors;
     var dump_button = ed.dump_button;
 
-    var demo = 0;
+    var demo = false;
     var loaded_file = false;
     function setEditorValue(val) {
         demo = false;
@@ -29,11 +29,11 @@ function load_editor() {
         demo_picker.appendChild(selection);
     }
     demo_picker.onchange = function() {
-        setEditorValue(file_values[demo_picker.selectedIndex]);
-        demo = file_values[demo_picker.selectedIndex];
+        load_demo(demo_picker.selectedIndex);
     };
     demo_picker.value = "0";
     setEditorValue(file_values[0]);
+    demo = 0;
 
     var ls_picker = document.getElementById("localstorage_picker");
     var files = JSON.parse(localStorage.getItem('__index__') || "[]");
@@ -49,28 +49,63 @@ function load_editor() {
     };
     document.getElementById("save_file").onclick = save_current;
     document.getElementById("new_file").onclick = add_file;
-    
-    function load_file(name) {
-        if (loaded_file != name) { save_current(); }
-        var new_val = localStorage.getItem(name);
-        setEditorValue(new_val);
-        loaded_file = name;
-    }
 
     function confirm_overwrite(name) {
         return window.confirm("File exists: Are you sure you want to over-write  \"" + name + "\"");
     }
-    function save_current() {
-        if(loaded_file) {
-            var changed = editor.getValue() !== localStorage.getItem(loaded_file);
-            var overwrite = !changed || confirm_overwrite(loaded_file);
-            if(!overwrite) { return false; }
-        } else {
-            loaded_file = window.prompt("save file as:","default");
-            if(localStorage.getItem(loaded_file) && !confirm_overwrite(loaded_file)) {
-                return false;
+    function pick_filename() {
+       return window.prompt("save file as:","default");
+    }
+    function confirm_save_progress() {
+        return window.confirm("Save progress before switching?");
+    }
+
+    function load_demo(id) {
+        var save_status = true;
+        if (demo !== id) { save_status = save_current(true); }
+        if(save_status) {
+            var demo_val = file_values[id];
+            setEditorValue(demo_val);
+            demo = id;
+        }
+    }
+
+    function load_file(name) {
+        var save_status = true;
+        if (loaded_file !== name) { save_status = save_current(true); }
+        if(save_status) {
+            var new_val = localStorage.getItem(name);
+            setEditorValue(new_val);
+            loaded_file = name;
+        }
+    }
+
+    function save_current(for_change) {
+        if (loaded_file !== false) {
+            if(editor.getValue() !== localStorage.getItem(loaded_file)) {
+
+                if(for_change === true && !confirm_save_progress()) {
+                    return true;
+                }
+
+                loaded_file = confirm_overwrite(loaded_file) ? loaded_file : false;
+            }
+        } else if (demo !== false) {
+            if (editor.getValue() !== file_values[demo]) {
+
+                if(for_change === true && !confirm_save_progress()) {
+                    return true;
+                }
+
+                loaded_file = window.prompt("save progress as: ", "default") || false;
+            } else if (for_change === true) {
+                return true;
             }
         }
+
+        if(!loaded_file) { return false; }
+        demo = false;
+
         if(files.indexOf(loaded_file) < 0) {
             files.push(loaded_file);
 
@@ -81,11 +116,10 @@ function load_editor() {
             localStorage.setItem('__index__', JSON.stringify(files));
         };
         localStorage.setItem(loaded_file, editor.getValue());
-    } 
+        return true;
+    }
 
     function add_file() {
-        save_current();
-        setEditorValue('');
-        save_current();
+        if(save_current(true)) { setEditorValue(''); }
     }
 }
